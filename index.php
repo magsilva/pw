edcf3ae26b7b1488b296e612a6176d9624d892a1
Marco Aurélio Graciotto Silva <magsilva@gmail.com>
2013-12-30 23:49:48 -0200

    Add initial configuration to integrate application with Facebook.

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
 
 require 'vendor/autoload.php';
 
+require 'vendor/facebook/php-sdk/src/facebook.php';
+
 date_default_timezone_set('UTC');
 
 use Monolog\Logger;
@@ -14,5 +16,23 @@ $log->pushHandler(new StreamHandler('pw.log', Logger::WARNING));
 // add records to the log
 $log->addWarning('Foo');
 $log->addError('Bar');
+
+
+$facebook = new Facebook(array(
+  'appId'  => '628634493869371',
+  'secret' => 'd2de4ae4eec14042d23daebe49d2b7bc',
+));
+
+// Get User ID
+$user = $facebook->getUser();
+if ($user) {
+  try {
+    // Proceed knowing you have a logged in user who's authenticated.
+    $user_profile = $facebook->api('/me');
+  } catch (FacebookApiException $e) {
+    error_log($e);
+    $user = null;
+  }
+}
 
 ?>
